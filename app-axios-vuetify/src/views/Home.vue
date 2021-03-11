6b807ad33b20654c0c67326295164d614dc22888
taniacaceres94 <taniacaceres1994@gmail.com>
2021-03-11 13:08:04 +0100

    cursito

---
+++ app-axios-vuetify/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data(){
+    return{
+      fecha: new Date().toISOString().substr(0,10)
+    }
+  },
+  methods: {
+    async getDolar(dia){
+
+      let datos = await axios.get(`https://mindicador.cl/api/dolar/${dia}`)
+      console.log(datos.data.serie[0].valor)
+    }
+  },
+  created(){
+    this.getDolar('01-02-2019')
+  }
+}
+</script>
---
+++ validar-form2/src/views/Validaciones.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Vuelidate</h1>
+    </div>
+</template>
+
+<script>
+
+import { required, email } from "vuelidate/lib/validators"
+export default {
+    name: "Validaciones"
+}
+</script>
